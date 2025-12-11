e58e83426b927bbe72e8cf866d521e57880c82b9
Bradley-Hill <brad.michael.hill@gmail.com>
2025-12-11 18:08:39 +0100

    OdinProject: added the dropdown with hardcoded sets to display

--- src/pages/Shopping.tsx
+++ src/pages/Shopping.tsx
@@ -1,13 +1,15 @@
 import { useEffect, useState } from "react";
 import { fetchRandomCards } from "../services/scryfallRandomRequest";
-import "../styles/Shopping.css";
+import { useShoppingContext } from "../context/ShoppingProvider";
 import Card from "../components/Card";
 import type { MagicCard } from "../types/MagicCard";
+import "../styles/Shopping.css";
 
 export default function Shopping() {
   const [cards, setCards] = useState<MagicCard[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
+  const { sets, currentSet,loading: contextLoading, error: contextError, selectSet } = useShoppingContext();;
 
   useEffect(() => {
     const loadCards = async () => {
@@ -23,12 +25,35 @@ export default function Shopping() {
     loadCards();
   }, []);
 
+  
+  const displayCards = currentSet && sets[currentSet] ? sets[currentSet] : cards;
+
   if (loading) return <div>Loading Cards...</div>;
   if (error) return <div>Error: {error}</div>;
   return (
     <div className="bg-shopping-page">
+      <select 
+        onChange={(e) => {
+          if(e.target.value) {
+            selectSet(e.target.value);
+          } else {
+            // Reset to random cards if no set is selected
+            setCards([]);
+          }
+        }} 
+        disabled={contextLoading}
+      >
+        <option value="">-- Select a Set --</option>
+        <option value="TOR">Torment</option>
+        <option value="JUD">Judgment</option>
+        <option value="ONS">Onslaught</option>
+        <option value="LGN">Legions</option>
+        <option value="LRW">Lorwyn</option>
+        <option value="SHM">Shadowmoor</option>
+        <option value="ZEN">Zendikar</option>
+      </select>
       <div className="cards-grid">
-        {cards.map((card) => (
+        {displayCards.map((card) => (
           <Card key={card.scryfall_id} card={card} />
         ))}
       </div>

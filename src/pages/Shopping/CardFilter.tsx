c910908cba1b73f3372e2aac64276c7cf90a063e
Bradley-Hill <brad.michael.hill@gmail.com>
2025-12-13 15:41:29 +0100

    OdinProject: added styling for the dropdown selector and filtering options

--- src/pages/Shopping/CardFilter.tsx
+++ src/pages/Shopping/CardFilter.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import type { CardFilterProps } from "../../types/FilterOptions";
+import "../../styles/CardFilter.css"
 
 export function CardFilter({ onFilterChange }: CardFilterProps) {
   const [colors, setColors] = useState<string[]>([]);
--- src/pages/Shopping/SetSelector.tsx
+++ src/pages/Shopping/SetSelector.tsx
@@ -1,33 +1,36 @@
 import { useShoppingContext } from "../../context/ShoppingProvider";
+import "../../styles/SetSelector.css"
 
 export function SetSelector() {
   const { loading: contextLoading, selectSet } = useShoppingContext();
 
   return (
-    <select
-      onChange={(e) => {
-        if (e.target.value) {
-          selectSet(e.target.value);
-        }
-      }}
-      disabled={contextLoading}
-    >
-      <option value="">-- Select a Set --</option>
-      <option value="TOR">Torment</option>
-      <option value="JUD">Judgment</option>
-      <option value="ONS">Onslaught</option>
-      <option value="LGN">Legions</option>
-      <option value="LRW">Lorwyn</option>
-      <option value="MOR">Morningtide</option>
-      <option value="SHM">Shadowmoor</option>
-      <option value="EVE">Eventide</option>
-      <option value="ZEN">Zendikar</option>
-      <option value="BLB">Bloomburrow</option>
-      <option value="MH3">Modern Horizons 3</option>
-      <option value="FDN">Foundations</option>
-      <option value="TDM">Tarkir: Dragonstorm</option>
-      <option value="EOE">Edge of Eternities</option>
-      <option value="ECL">Lorwyn: Eclipsed</option>
-    </select>
+    <div className="setSelector-container">
+      <select
+        onChange={(e) => {
+          if (e.target.value) {
+            selectSet(e.target.value);
+          }
+        }}
+        disabled={contextLoading}
+      >
+        <option value="">-- Select a Set --</option>
+        <option value="TOR">Torment</option>
+        <option value="JUD">Judgment</option>
+        <option value="ONS">Onslaught</option>
+        <option value="LGN">Legions</option>
+        <option value="LRW">Lorwyn</option>
+        <option value="MOR">Morningtide</option>
+        <option value="SHM">Shadowmoor</option>
+        <option value="EVE">Eventide</option>
+        <option value="ZEN">Zendikar</option>
+        <option value="BLB">Bloomburrow</option>
+        <option value="MH3">Modern Horizons 3</option>
+        <option value="FDN">Foundations</option>
+        <option value="TDM">Tarkir: Dragonstorm</option>
+        <option value="EOE">Edge of Eternities</option>
+        <option value="ECL">Lorwyn: Eclipsed</option>
+      </select>
+    </div>
   );
 }
